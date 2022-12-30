18f6e1a04a4e412a90ffb46d06c47b74338aabba
Mat-Longinow <mat.longinow@gmail.com>
2022-12-30 13:18:39 -0800

    Added YouTube Link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,7 @@ import navLogo from './../assets/nav-logo.png'
 import { useHistory, Link } from "react-router-dom";
 import { setCurrentNavLink } from "../util";
 import discordLogo from './../assets/nav/discordLogo.png'
+import youtubeLogo from './../assets/nav/youtube-logo.png'
 
 
 const { Header } = Layout;
@@ -11,6 +12,7 @@ const { Header } = Layout;
 const Nav = () => {
 	let history = useHistory()
 	const discordLink = 'https://discord.gg/kJ4dsnQJBy'
+	const youtubeLink = 'https://www.youtube.com/@kinsmanredeemer8045/featured'
 
 	useEffect(() => {
 		setCurrentNavLink()
@@ -18,7 +20,7 @@ const Nav = () => {
 
 	return (
 		<Header id="navbar">
-			<img className="logo" src={navLogo} />
+			<img id="guildLogo" className="logo" src={navLogo} />
 
 			<Row>
 				<Col>
@@ -46,8 +48,14 @@ const Nav = () => {
 					</div>
 
 					<div className="menu-item">
+						<a href={youtubeLink}>
+							<img id="youtubeLogo" src={youtubeLogo}/>
+						</a>
+					</div>
+
+					<div className="menu-item">
 						<a href={discordLink}>
-							<img src={discordLogo}/>
+							<img id="discordLogo" src={discordLogo}/>
 						</a>
 					</div>
 				</Col>
